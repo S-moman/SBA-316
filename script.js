d6ca1ced67acec1cc83d5bf1b1c2786a0890becc
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-09-17 20:45:10 -0400

    table update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let serviceContainer = document.getElementById("serviceBlock")
-const menu = document.getElementById('menu');
-menu.style.backgroundColor = 'blue'
-menu.style.color ='orange'
-menu.style.height = '100%';
+let serviceContainer = document.getElementById("serviceBlock");
+const menu = document.getElementById("menu");
+menu.style.backgroundColor = "blue";
+menu.style.color = "orange";
+menu.style.height = "100%";
 menu.classList.add("flex-around");
 menu.classList.add("flex-ctr");
 
 
 const menuLinks = [
-    {text: 'Home', href: '#'},
-    {text: 'Register', href: '#'},
-    {text: 'Contact', href: '#'},
-    {text: 'Services', href: '#'},
-    {text: 'Log In', href: '#' },
-]
+  { text: "Home", href: "#" },
+  { text: "Register", href: "#" },
+  { text: "Contact", href: "#" },
+  { text: "Services", href: "#" },
+  { text: "Log In", href: "#" },
+];
 
 // let menuLink = menuLinks.length
 
@@ -25,23 +25,22 @@ const menuLinks = [
 // }
 
 menuLinks.forEach((menuLink) => {
-    let anchor = document.createElement('a');
-    anchor.setAttribute('href', menuLink.href);
-    anchor.innerHTML = menuLink.text;
-    menu.appendChild(anchor);
-})
-
-
+  const anchor = document.createElement("a");
+  anchor.setAttribute("href", menuLink.href);
+  anchor.innerHTML = menuLink.text;
+  menu.appendChild(anchor);
+});
 
 function createContainer() {
-    for(let i = 0; i < 3; i++) {
-        let box = document.createElement("div");
-        
-            serviceContainer.appendChild(box);
+  for (let i = 0; i < 1; i++) {
+    let box = document.createElement("div");
+    serviceContainer.appendChild(box);
+    let list = document.createElement("ol")
+    box.appendChild(list)
+    for(j = 0; j < 40; j++) {
+        let listItems = document.createElement("li")
+        list.appendChild(listItems)
     }
-} 
-createContainer()
-
-
-
-
+  }
+}
+createContainer();
